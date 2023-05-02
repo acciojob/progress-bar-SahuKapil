7df492ec2345a1e57731f93dc2f891c63b539bfb
Kapil Sahu <116225142+SahuKapil@users.noreply.github.com>
2023-05-03 01:46:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+let currentActive = 1;
+
+nextBtn.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
+});
+
+prevBtn.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  if (currentActive === 1) {
+    prevBtn.disabled = true;
+  } else if (currentActive === circles.length) {
+    nextBtn.disabled = true;
+  } else {
+    prevBtn.disabled = false;
+    nextBtn.disabled = false;
+  }
+}
